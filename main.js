81205a44f0670b48fd432292bbee6276550486d3
Abhishek Rijal <abhishek.rijal1999@gmail.com>
2023-06-09 00:15:33 +0545

    style(main): format

--- main.js
+++ main.js
@@ -1,170 +1,168 @@
-import './style.css'
+import "./style.css";
 
 window.onload = function () {
   document
-    .querySelector('.add-new')
-    .addEventListener('click', addNewSubjectElement)
+    .querySelector(".add-new")
+    .addEventListener("click", addNewSubjectElement);
   document
-    .querySelector('.calculate')
-    .addEventListener('click', calculateSemesterEndGradePointAverage)
+    .querySelector(".calculate")
+    .addEventListener("click", calculateSemesterEndGradePointAverage);
 
   function addNewSubjectElement() {
-    const form = document.querySelector('.form')
-    let mainDiv = document.createElement('div')
-    mainDiv.classList.add('form-group')
-    mainDiv.appendChild(
-      createInputType('subject', 'Enter subject for identification'),
-    )
-    mainDiv.appendChild(createInputType('credit', 'Enter credit hour*'))
-    mainDiv.appendChild(createSelectElement())
-    mainDiv.appendChild(createCloseElement())
-    form.appendChild(mainDiv)
+    const form = document.querySelector(".form");
+    let mainDiv = document.createElement("div");
+    mainDiv.classList.add("form-group");
+    mainDiv.appendChild(createInputType("subject", "Subject"));
+    mainDiv.appendChild(createInputType("credit", "Credit hours*"));
+    mainDiv.appendChild(createSelectElement());
+    mainDiv.appendChild(createCloseElement());
+    form.appendChild(mainDiv);
     // attach close event on newly created element
-    let allCloseButton = document.querySelectorAll('.close')
+    let allCloseButton = document.querySelectorAll(".close");
     allCloseButton.forEach((element) => {
-      element.addEventListener('click', destroyElement)
-    })
+      element.addEventListener("click", destroyElement);
+    });
   }
 
   function createInputType(name, placeholder) {
-    let inputType = document.createElement('input')
+    let inputType = document.createElement("input");
     Object.assign(inputType, {
-      type: 'text',
+      type: "text",
       name,
       id: name,
       placeholder,
-    })
-    return inputType
+    });
+    return inputType;
   }
 
   function createSelectElement() {
-    let selectElement = document.createElement('select')
-    selectElement.setAttribute('name', 'grade')
-    selectElement.setAttribute('id', 'grade')
-    let selectPlaceholder = document.createElement('option')
-    selectPlaceholder.setAttribute('selected', 'selected')
-    selectPlaceholder.setAttribute('disabled', 'disabled')
-    selectPlaceholder.text = 'Select Your Grade*'
-    selectElement.add(selectPlaceholder)
-    let letterGrades = ['A+', 'A', 'B+', 'B', 'C', 'D', 'F']
+    let selectElement = document.createElement("select");
+    selectElement.setAttribute("name", "grade");
+    selectElement.setAttribute("id", "grade");
+    let selectPlaceholder = document.createElement("option");
+    selectPlaceholder.setAttribute("selected", "selected");
+    selectPlaceholder.setAttribute("disabled", "disabled");
+    selectPlaceholder.text = "Select Your Grade*";
+    selectElement.add(selectPlaceholder);
+    let letterGrades = ["A+", "A", "B+", "B", "C", "D", "F"];
     letterGrades.map((eachLetterGrade) => {
-      let createOption = document.createElement('option')
-      createOption.value = eachLetterGrade
-      createOption.text = eachLetterGrade
-      selectElement.add(createOption)
-    })
-    return selectElement
+      let createOption = document.createElement("option");
+      createOption.value = eachLetterGrade;
+      createOption.text = eachLetterGrade;
+      selectElement.add(createOption);
+    });
+    return selectElement;
   }
 
   function createCloseElement() {
-    let closeButton = document.createElement('button')
-    closeButton.setAttribute('type', 'button')
-    closeButton.classList.add('close')
-    closeButton.innerHTML = '&times;'
-    return closeButton
+    let closeButton = document.createElement("button");
+    closeButton.setAttribute("type", "button");
+    closeButton.classList.add("close");
+    closeButton.innerHTML = "&times;";
+    return closeButton;
   }
 
   function destroyElement(event) {
-    event.target.parentNode.remove()
+    event.target.parentNode.remove();
   }
 
   function calculateSemesterEndGradePointAverage() {
-    let preparedData = []
-    let errorEncountered = false
-    let allFormGroupElements = document.querySelectorAll('.form-group')
+    let preparedData = [];
+    let errorEncountered = false;
+    let allFormGroupElements = document.querySelectorAll(".form-group");
     allFormGroupElements.forEach((element) => {
-      let subject = element.querySelector('input#subject')
-      let credit = element.querySelector('input#credit')
-      let grade = element.querySelector('select#grade')
+      let subject = element.querySelector("input#subject");
+      let credit = element.querySelector("input#credit");
+      let grade = element.querySelector("select#grade");
       preparedData.push({
         subject: subject.value,
         credit: parseInt(credit.value),
         letterGrade: grade.value.toUpperCase(),
         grade: letterGradeToGradeValueConversion(grade.value.toUpperCase()),
-      })
-    })
+      });
+    });
     let totalCredit = preparedData.reduce(
       (currentValue, data) => currentValue + data.credit,
-      0,
-    )
+      0
+    );
     if (Number.isNaN(totalCredit)) {
-      errorEncountered = true
+      errorEncountered = true;
     }
-    let mulCreditAndGradePoint = 0
+    let mulCreditAndGradePoint = 0;
     preparedData.forEach((data) => {
-      mulCreditAndGradePoint += data.credit * data.grade
-    })
+      mulCreditAndGradePoint += data.credit * data.grade;
+    });
     if (Number.isNaN(mulCreditAndGradePoint)) {
-      errorEncountered = true
+      errorEncountered = true;
     }
-    let sgpa = mulCreditAndGradePoint / totalCredit
+    let sgpa = mulCreditAndGradePoint / totalCredit;
     if (Number.isNaN(sgpa)) {
-      errorEncountered = true
+      errorEncountered = true;
     }
-    createTableBody(preparedData)
-    updateTableFooter(totalCredit)
-    document.querySelector('.sgpa').textContent = sgpa.toFixed(2)
-    showContainer(errorEncountered)
+    createTableBody(preparedData);
+    updateTableFooter(totalCredit);
+    document.querySelector(".sgpa").textContent = sgpa.toFixed(2);
+    showContainer(errorEncountered);
   }
 
   function createTableBody(tableData) {
-    const tableBody = document.querySelector('tbody')
+    const tableBody = document.querySelector("tbody");
     // remove pre-existing table row(s)
     if (tableBody.childNodes.length > 0) {
       tableBody
-        .querySelectorAll('*')
-        .forEach((eachElement) => eachElement.remove())
+        .querySelectorAll("*")
+        .forEach((eachElement) => eachElement.remove());
     }
     tableData.forEach((data, index) => {
-      let tableRow = tableBody.insertRow()
-      let sn = tableRow.insertCell(0) // serial number
-      let subject = tableRow.insertCell(1)
-      let credit = tableRow.insertCell(2)
-      let letterGrade = tableRow.insertCell(3)
-      sn.textContent = ++index
-      subject.textContent = data.subject === '' ? '-' : data.subject
-      credit.textContent = data.credit
-      letterGrade.textContent = data.letterGrade
-    })
+      let tableRow = tableBody.insertRow();
+      let sn = tableRow.insertCell(0); // serial number
+      let subject = tableRow.insertCell(1);
+      let credit = tableRow.insertCell(2);
+      let letterGrade = tableRow.insertCell(3);
+      sn.textContent = ++index;
+      subject.textContent = data.subject === "" ? "-" : data.subject;
+      credit.textContent = data.credit;
+      letterGrade.textContent = data.letterGrade;
+    });
   }
 
   function updateTableFooter(totalCredits) {
-    let tableFooter = document.querySelector('tfoot')
-    let tableRow = tableFooter.querySelectorAll('td')
-    tableRow[1].innerText = totalCredits
+    let tableFooter = document.querySelector("tfoot");
+    let tableRow = tableFooter.querySelectorAll("td");
+    tableRow[1].innerText = totalCredits;
   }
 
   function letterGradeToGradeValueConversion(letterGrade) {
     return {
-      'A+': 4,
+      "A+": 4,
       A: 3.75,
-      'B+': 3.5,
+      "B+": 3.5,
       B: 3,
       C: 2.5,
       D: 1.75,
       F: 0,
-    }[letterGrade]
+    }[letterGrade];
   }
 
   function showContainer(isError) {
-    let container = document.querySelector('.response')
-    if (container.classList.contains('hidden')) {
-      container.classList.toggle('hidden')
+    let container = document.querySelector(".response");
+    if (container.classList.contains("hidden")) {
+      container.classList.toggle("hidden");
     }
-    let successElement = document.querySelector('.when-success')
-    let errorElement = document.querySelector('.when-error')
+    let successElement = document.querySelector(".when-success");
+    let errorElement = document.querySelector(".when-error");
     if (isError) {
-      if (!successElement.classList.contains('hidden')) {
-        successElement.classList.add('hidden')
+      if (!successElement.classList.contains("hidden")) {
+        successElement.classList.add("hidden");
       }
-      if (errorElement.classList.contains('hidden')) {
-        errorElement.classList.remove('hidden')
+      if (errorElement.classList.contains("hidden")) {
+        errorElement.classList.remove("hidden");
       }
     } else {
-      if (!errorElement.classList.contains('hidden')) {
-        errorElement.classList.add('hidden')
+      if (!errorElement.classList.contains("hidden")) {
+        errorElement.classList.add("hidden");
       }
-      successElement.classList.remove('hidden')
+      successElement.classList.remove("hidden");
     }
   }
-}
+};
